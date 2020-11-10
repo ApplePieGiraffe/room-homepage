6ac1accb0122eeb413a37587200f39aa8cfa4f75
ApplePieGiraffe <itsmejoshwoodson@gmail.com>
2020-11-10 15:08:52 +0300

    Another commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 const menuBtn = document.querySelector('.menu-btn');
+const headerNav = document.querySelector('.header__nav');
+const filter = document.querySelector('.filter');
 
 function toggleMenuBtn() {
     menuBtn.classList.toggle('menu-btn--active');
 }
 
-menuBtn.addEventListener('click', toggleMenuBtn);
+function displayNav() {
+    headerNav.classList.toggle('header__nav--active');
+}
+
+function toggleFilter() {
+    filter.classList.toggle('filter--active');
+}
+
+function toggleNav() {
+    toggleMenuBtn();
+    displayNav();
+    toggleFilter();
+}
+
+menuBtn.addEventListener('click', toggleNav);
