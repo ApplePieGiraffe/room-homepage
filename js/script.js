56ddd41ad183ce709626cfef56aeae3d1fc7731d
ApplePieGiraffe <itsmejoshwoodson@gmail.com>
2020-11-11 13:56:15 +0300

    Get image carousel working

--- js/script.js
+++ js/script.js
@@ -27,3 +27,28 @@ function toggleNav() {
 }
 
 menuBtn.addEventListener('click', toggleNav);
+
+// CAROUSEL
+
+const images = document.querySelectorAll('.carousel-img');
+const prevBtn = document.querySelector('.slider-btn--prev');
+const nextBtn = document.querySelector('.slider-btn--next');
+let index = 0;
+
+function prev() {
+    if (index === 0) {
+        index = 3;
+    }
+    images[index % images.length].classList.replace('header__img--active', 'header__img--inactive');
+    images[(index - 1) % images.length].classList.replace('header__img--inactive', 'header__img--active');
+    index--;
+}
+
+function next() {
+    images[index % images.length].classList.replace('header__img--active', 'header__img--inactive');
+    images[(index + 1) % images.length].classList.replace('header__img--inactive', 'header__img--active');
+    index++;
+}
+
+prevBtn.addEventListener('click', prev);
+nextBtn.addEventListener('click', next);
