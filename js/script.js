14653222d1ff42c75da7fdc9aa9aa88a448e06e5
ApplePieGiraffe <itsmejoshwoodson@gmail.com>
2020-11-11 11:49:37 +0300

    Just need to add carousel

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
+// MOBILE NAVIGATION
+
 const menuBtn = document.querySelector('.menu-btn');
 const headerNav = document.querySelector('.header__nav');
-const filter = document.querySelector('.filter');
+const filter = document.querySelector('.bg-filter');
 
 function toggleMenuBtn() {
     menuBtn.classList.toggle('menu-btn--active');
 }
 
 function displayNav() {
-    headerNav.classList.toggle('header__nav--active');
+    if (headerNav.classList.contains('header__nav--inactive')) {
+        headerNav.classList.replace('header__nav--inactive', 'header__nav--active');
+    } else {
+        headerNav.classList.replace('header__nav--active', 'header__nav--inactive');
+    }
 }
 
 function toggleFilter() {
-    filter.classList.toggle('filter--active');
+    filter.classList.toggle('bg-filter--active');
 }
 
 function toggleNav() {
