0b0b45fbf451a4bca8ac108dcf388484940f49d2
ApplePieGiraffe <itsmejoshwoodson@gmail.com>
2020-11-12 11:54:22 +0300

    Wrap up project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+// JS ANIMATIONS
+
+const timeline = gsap.timeline({defaults: {ease: 'power1.out'}});
+
+timeline.to('.intro__text', {y: '0%', duration: 1, stagger: 0.25});
+timeline.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5});
+timeline.to('.intro', {y: '-100%', duration: 1}, "-=1");
+
 // MOBILE NAVIGATION
 
 const body = document.querySelector('body');
